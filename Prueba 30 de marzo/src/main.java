ec6bf4af86e6a04af1b01f4325cb342f2f76b9f5
DenilsonRamirez <denixmas123@gmail.com>
2020-03-30 21:21:54 -0600

    Update main.java

--- Prueba 30 de marzo/src/main.java
+++ Prueba 30 de marzo/src/main.java
@@ -6,6 +6,11 @@ public class main {
        System.out.println("Nombre: José Alejandro López Ramírez");
        System.out.println("Direccion: Huehuetenango Zona 8");
        System.out.println("Telefono: 55928636");
+        
+        
+       System.out.println("Nombre: Denilson Baltazar Ramírez Andrés");
+       System.out.println("Direccion: Barillas Zona 6");
+       System.out.println("Telefono: 46400006");
        
     }
     
