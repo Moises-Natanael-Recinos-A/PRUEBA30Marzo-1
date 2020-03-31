29d6aff9972e910b78d9a92be782a879504f79f7
PC <PC@LAPTOP-G3KKDKU7.lan>
2020-03-30 20:56:42 -0600

    Segunda modificación Prueba

--- Prueba 30 de marzo/src/main.java
+++ Prueba 30 de marzo/src/main.java
@@ -1,18 +1,7 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 
-/**
- *
- * @author PC
- */
 public class main {
 
-    /**
-     * @param args the command line arguments
-     */
+
     public static void main(String[] args) {
        System.out.println("Nombre: José Alejandro López Ramírez");
        System.out.println("Direccion: Huehuetenango Zona 8");
