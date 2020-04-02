7776ecbbe0df213c32126cead5e6ad813cfc5c96
Victor Tejeda <62724633+victorTe133@users.noreply.github.com>
2020-04-02 15:38:51 -0600

    Update main.java

--- Prueba 30 de marzo/src/main.java
+++ Prueba 30 de marzo/src/main.java
@@ -11,6 +11,10 @@ public class main {
        System.out.println("Nombre: Denilson Baltazar Ramírez Andrés");
        System.out.println("Direccion: Barillas Zona 6");
        System.out.println("Telefono: 46400006");
+        
+       System.out.println("Nombre: Victor Daniel Tejeda Hernández");
+       System.out.println("Direccion: Huehuetenango Zona 8");
+       System.out.println("Telefono: 34175073");
        
     }
     
